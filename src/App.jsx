57bc1fa6cbe8918c47d542c13410dfec7e92b75e
Kevin <estivenbueno7@gmail.com>
2026-08-13 22:21:46 -0500

    ya cree mi primer hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import Hero from './components/Hero';
+
 function App() {
   return (
-    <h1>Hola, soy Kevin</h1>
+    <>
+      <Hero />
+    </>
   );
 }
 
---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+function Hero() {
+  return (
+    <section>
+      <h1>Hola, soy Kevin Bueno</h1>
+      <h2>Junior Web Developer</h2>
+      <p>
+        Estoy construyendo soluciones web mientras desarrollo
+        mis habilidades en programación.
+      </p>
+    </section>
+  );
+}
+
+export default Hero;
